2a976c24fa0e5215af326829d591330c415a4c72
Hamza Mezerreg <hamza.mez@gmail.com>
2016-07-11 13:36:14 +0100

    Defined (lat? l) for list of atoms

--- thelittleschemer/thelittleschemer.scm
+++ thelittleschemer/thelittleschemer.scm
@@ -2,3 +2,14 @@
 (define atom?
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
+
+;; (lat? l) is l a list?
+;; the defenition can be found chapter 2, page 16.
+;; there is no error handling, if you don't pass a list or
+;; a list of lists and error will occur.
+(define lat?
+  (lambda (l)
+    (cond
+     ((null? l) #t)
+     ((atom? (car l)) (lat? (cdr l)))
+     (else #f))))
