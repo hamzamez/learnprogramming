b8a299cbd81cd83552299f03bec31f93b53de708
Hamza Mezerreg <hamza.mez@gmail.com>
2016-07-11 13:38:04 +0100

    Fixed some typos

--- thelittleschemer/thelittleschemer.scm
+++ thelittleschemer/thelittleschemer.scm
@@ -3,8 +3,8 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 
-;; (lat? l) is l a list?
-;; the defenition can be found chapter 2, page 16.
+;; (lat? l) is l a list of atoms ?
+;; the definition can be found chapter 2, page 16.
 ;; there is no error handling, if you don't pass a list or
 ;; a list of lists and error will occur.
 (define lat?
